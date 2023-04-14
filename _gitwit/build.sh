ea927bf3baf0d053734d4eb5c1b716f9226583a8
Gitwit <git@gitwit.dev>
2023-04-14 13:52:06 +0000

    Finished code generation, adding logs

--- _gitwit/build.sh
+++ _gitwit/build.sh
@@ -1,94 +1,99 @@
 #!/bin/sh
 
-# Initialize git repo
-git init
-echo -e "node_modules/" > .gitignore
-echo -e ".DS_Store" >> .gitignore
-echo -e "package-lock.json" >> .gitignore
-echo -e "README.md" >> .gitignore
-git add .gitignore
-git commit -a -m "👷🏼 Add .gitignore"
+# Add README.md
+echo -e -e "# NodeJS API that takes two inputs" > README.md
+echo -e -e "" >> README.md
+echo -e -e "This server can be used as an intermediary to cache HTTP responses from a given URL. It listens on port 3000 by default, but that can be changed by setting the PORT environment variable." >> README.md
+echo -e -e "" >> README.md
+echo -e -e "## How to use" >> README.md
+echo -e -e "" >> README.md
+echo -e -e "To start the server, run `npm start`" >> README.md
+echo -e -e "" >> README.md
+echo -e -e "To request a cached or non-cached response, send a GET request to `http://localhost:3000/cache?url=YOUR_URL&expiration=SECONDS_TO_EXPIRE`" >> README.md >> README.md
+echo -e -e "" >> README.md
+git add README.md
+git commit -a -m "👷🏼 Add README.md"
 
-# Create package.json
-echo -e "{}" > package.json
+# Install dependencies
+npm install axios express express-rate-limit morgan
 git add package.json
-git commit -a -m "👷🏼 Add package.json"
+git commit -a -m "👷🏼 Install dependencies"
 
 # Initialize NodeJS project
 npm init -y
-git add .
+git add package.json
 git commit -a -m "👷🏼 Initialize NodeJS project"
 
-# Install dependencies
-npm install --save express axios morgan express-rate-limit
-git add package.json package-lock.json
-git commit -a -m "👷🏼 Install dependencies"
+# Add server.js
+echo -e -e "const express = require('express');
+const axios = require('axios');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const app = express();
 
-# Create server.js file
-echo -e "const express = require('express');" >> server.js
-echo -e "const axios = require('axios');" >> server.js
-echo -e "const morgan = require('morgan');" >> server.js
-echo -e "const rateLimit = require('express-rate-limit');" >> server.js
-echo -e "const app = express();" >> server.js
-echo -e "" >> server.js
-echo -e "const cache = new Map();" >> server.js
-echo -e "" >> server.js
-echo -e "app.use(morgan('tiny'));" >> server.js
-echo -e "" >> server.js
-echo -e "const limiter = rateLimit({" >> server.js
-echo -e "    windowMs: 60 * 1000," >> server.js
-echo -e "    max: 10," >> server.js
-echo -e "    message: 'Too many requests, please try again later.'" >> server.js
-echo -e "});" >> server.js
-echo -e "app.use(limiter);" >> server.js
-echo -e "" >> server.js
-echo -e "app.get('/cache', async (req, res) => {" >> server.js
-echo -e "    const url = req.query.url;" >> server.js
-echo -e "    const expiration = parseInt(req.query.expiration);" >> server.js
-echo -e "    const isCache = cache.has(url);" >> server.js
-echo -e "" >> server.js
-echo -e "    if (isCache) {" >> server.js
-echo -e "        const cachedResponse = cache.get(url);" >> server.js
-echo -e "        const now = new Date().getTime();" >> server.js
-echo -e "" >> server.js
-echo -e "        if (now > cachedResponse.expiration) {" >> server.js
-echo -e "            cache.delete(url);" >> server.js
-echo -e "        } else {" >> server.js
-echo -e "            return res.status(cachedResponse.status).send(cachedResponse.data);" >> server.js
-echo -e "        }" >> server.js
-echo -e "    }" >> server.js
-echo -e "" >> server.js
-echo -e "    try {" >> server.js
-echo -e "        const response = await axios.get(url);" >> server.js
-echo -e "        cache.set(url, {" >> server.js
-echo -e "            status: response.status," >> server.js
-echo -e "            expiration: new Date().getTime() + expiration * 1000," >> server.js
-echo -e "            data: response.data" >> server.js
-echo -e "        });" >> server.js
-echo -e "" >> server.js
-echo -e "        return res.status(response.status).send(response.data);" >> server.js
-echo -e "    } catch (error) {" >> server.js
-echo -e "        console.error(error);" >> server.js
-echo -e "        return res.status(500).send('Error fetching data.');" >> server.js
-echo -e "    }" >> server.js
-echo -e "});" >> server.js
-echo -e "" >> server.js
+const cache = new Map();
+
+app.use(morgan('tiny'));
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 10,
+    message: 'Too many requests, please try again later.'
+});
+app.use(limiter);
+
+app.get('/cache', async (req, res) => {
+    const url = req.query.url;
+    const expiration = parseInt(req.query.expiration);
+    const isCache = cache.has(url);
+
+    if (isCache) {
+        const cachedResponse = cache.get(url);
+        const now = new Date().getTime();
+
+        if (now > cachedResponse.expiration) {
+            cache.delete(url);
+        } else {
+            return res.status(cachedResponse.status).send(cachedResponse.data);
+        }
+    }
+
+    try {
+        const response = await axios.get(url);
+        cache.set(url, {
+            status: response.status,
+            expiration: new Date().getTime() + expiration * 1000,
+            data: response.data
+        });
+
+        return res.status(response.status).send(response.data);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error fetching data.');
+    }
+});
+" > server.js
 git add server.js
 git commit -a -m "👷🏼 Add server.js"
 
-# Create README.md
-echo -e "# NodeJS API that takes two inputs" > README.md
-echo -e "" >> README.md
-echo -e "This server can be used as an intermediary to cache HTTP responses from a given URL. It listens on port 3000 by default, but that can be changed by setting the PORT environment variable." >> README.md
-echo -e "" >> README.md
-echo -e "## How to use" >> README.md
-echo -e "" >> README.md
-echo -e "To start the server, run `npm start`" >> README.md
-echo -e "" >> README.md
-echo -e "To request a cached or non-cached response, send a GET request to `http://localhost:3000/cache?url=YOUR_URL&expiration=SECONDS_TO_EXPIRE`" >> README.md
-echo -e "" >> README.md
-echo -e "- `url` (required): The URL of the resource to fetch." >> README.md
-echo -e "- `expiration` (required): The number of seconds to keep the cached response." >> README.md
-echo -e "" >> README.md
-git add README.md
-git commit -a -m "👷🏼 Add README.md"
+# Add .gitignore
+echo -e -e "node_modules/
+.DS_Store
+package-lock.json
+README.md" > .gitignore
+git add .gitignore
+git commit -a -m "👷🏼 Add .gitignore"
+
+# Add info.json
+echo -e -e '{
+    "repositoryName": "nodejs-api-that-takes-two-inputs",
+    "description": "A NodeJS API that takes two inputs: A web URL, and an expiration period. Upon receiving a request, it checks for a cached HTTP response from that URL. If available and within the expiration period, it returns the response. Otherwise, it fetches the response and stores it in the cache.",
+    "generator": "GitWit",
+    "generatorVersion": "0.0.6",
+    "gptModel": "gpt-3.5-turbo-0301",
+    "completionId": "chatcmpl-75E3PZGbKZD861C4x8vGLF23X1VXE",
+    "repositoryURL": "https://github.com/gitwitapp/nodejs-api-that-takes-two-inputs.git",
+    "dateCreated": "2023-04-14T13:47:50.953Z"
+}' > _gitwit/info.json
+git add _gitwit/info.json
+git commit -a -m "👷🏼 Add info.json"
